649f4b71bf6db3c3d91da9922c7dbfc290b1661f
Jorus Junio <jorusjunio28@gmail.com>
2026-05-18 22:14:51 +0800

    Fix scrolling of website

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="min-h-screen overflow-x-hidden bg-[#0A0A0A]">
+    <main className="min-h-screen overflow-x-clip bg-[#0A0A0A]">
       <Navbar />
       <Hero />
       <About />
